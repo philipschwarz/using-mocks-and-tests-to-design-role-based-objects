7a206460be3dfbd42f0c3c45193096e771f84031
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-06-14 16:09:34 +0100

    code simplest implementation that makes the new test pass

--- main/test/CommandParser.java
+++ main/test/CommandParser.java
@@ -1,15 +1,24 @@
 public class CommandParser
 {
+    private static final String END_SALE_COMMAND = "EndSale";
+
     private final SaleEventListener saleEventListener;
 
     public CommandParser(SaleEventListener saleEventListener)
     {
-
         this.saleEventListener = saleEventListener;
     }
 
-    public void parse(String saleCommand)
+    public void parse(String messageFromDevice)
     {
-        saleEventListener.newSaleInitiated();
+        String commandName = messageFromDevice.split(":")[1].trim();
+        if(END_SALE_COMMAND.equals(commandName))
+        {
+            saleEventListener.saleCompleted();
+        }
+        else
+        {
+            saleEventListener.newSaleInitiated();
+        }
     }
 }
