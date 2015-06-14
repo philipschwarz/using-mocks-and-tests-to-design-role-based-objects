020f0863ada4bc50e81d51bd314a084befba8457
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-06-14 15:31:39 +0100

    define the SaleEventListener role

--- main/test/CommandParserTests.java
+++ main/test/CommandParserTests.java
@@ -1,12 +1,25 @@
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CommandParserTests
 {
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+
     @Test
     public void notifies_listener_of_new_sale_event()
     {
+        final SaleEventListener saleEventListener = context.mock(SaleEventListener.class);
         CommandParser commandParser = new CommandParser();
         String newSaleCommand = "Command:NewSale";
+        context.checking(new Expectations() {{
+            oneOf (saleEventListener).newSaleInitiated();
+        }});
+
         commandParser.parse(newSaleCommand);
     }
 }
